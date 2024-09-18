9eb2969d23ed5352280faa48c2b94298df88c5b3
DhominickJ <dhominickjohn.billena@wvsu.edu.ph>
2024-09-18 16:21:05 +0800

    Initial Commit: Application UI Design with Widgets

---
+++ android/app/src/main/kotlin/com/example/wergets/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wergets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/images/image_class.dart
@@ -0,0 +1,9 @@
+class ImageClass {
+  static const business = 'assets/images/Business.png';
+  static const delivery = 'assets/images/Delivery.png';
+  static const happy = 'assets/images/Happy.png';
+  static const logo = 'assets/images/Logo.png';
+  static const sad = 'assets/images/Sad.png';
+} 
+
+// RemBESTGirl :)
